53efccd490b440bcdcfa773b5a42d2804ea9c4ba
SourCandy21 <abcdmartins40@gmail.com>
2026-07-09 17:22:43 -0300

    Mudanças para corrigir os erros de tamanho

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './pages/navbar/navbar'
+import CadastroItem from './pages/CadastroItem.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
-  </React.StrictMode>
-)
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import CadastroItem from './pages/CadastroItem.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <CadastroItem />
-  </StrictMode>,
+  </React.StrictMode>
 )
