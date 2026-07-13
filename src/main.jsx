ca2c89dfb5f8102579fa6bf963738d3a94204b19
Jamersonze <jamersonze54@gmail.com>
2026-07-13 00:21:14 -0300

    refactor: CadastroItem and Navbar components; update import paths and remove unused files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navbar from './pages/navbar/navbar'
-import CadastroItem from './pages/CadastroItem.jsx'
+import Navbar from './components/navbar/Navbar.jsx'
+import CadastroItem from './pages/CadastroItem/CadastroItem.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
